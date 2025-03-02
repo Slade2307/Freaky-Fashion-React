b4dd79cc4ca6db0820a99d42a94a7d1edb0de35e
Slade <>
2025-03-02 21:39:56 +0100

    Resolved merge conflict in routes/index.js

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,9 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-const sqlite3 = require('sqlite3').verbose(); // Import sqlite3
+const sqlite3 = require("sqlite3").verbose(); // Import sqlite3
 
-<<<<<<< HEAD
-const db = new sqlite3.Database('./db/products.db');  // Connect to products database
-
-/* GET home page. */
-router.get('/', function(req, res, next) {
-=======
 // Connect to the database
-const db = new sqlite3.Database('./db/products.db', (err) => {
+const db = new sqlite3.Database("./db/products.db", (err) => {
   if (err) {
     console.error("Error connecting to the database:", err.message);
   } else {
@@ -18,27 +12,18 @@ const db = new sqlite3.Database('./db/products.db', (err) => {
 });
 
 /* GET home page. */
-router.get('/', (req, res) => {
->>>>>>> 899ac96f48ee06a730cb707a43945c9b24c1f5d4
+router.get("/", (req, res) => {
   db.all("SELECT * FROM products", [], (err, rows) => {
     if (err) {
       console.error("Failed to retrieve products:", err.message);
       return res.status(500).send("Failed to retrieve products");
     }
-<<<<<<< HEAD
-    res.render('index', { title: 'Freaky Fashion', products: rows });  // Pass products to EJS
-=======
-    res.render('index', { title: 'Freaky Fashion', products: rows }); // Pass products to EJS
->>>>>>> 899ac96f48ee06a730cb707a43945c9b24c1f5d4
+    res.render("index", { title: "Freaky Fashion", products: rows }); // Pass products to EJS
   });
 });
 
 /* GET product details page by slug */
-<<<<<<< HEAD
-router.get('/product/:slug', function(req, res, next) {
-=======
-router.get('/product/:slug', (req, res) => {
->>>>>>> 899ac96f48ee06a730cb707a43945c9b24c1f5d4
+router.get("/product/:slug", (req, res) => {
   const productSlug = req.params.slug;
 
   // Fetch the product based on the slug from the database
@@ -54,21 +39,9 @@ router.get('/product/:slug', (req, res) => {
         console.error("Failed to retrieve similar products:", err.message);
         return res.status(500).send("Failed to retrieve similar products");
       }
-<<<<<<< HEAD
-      res.render('product-details', { product: product, similarProducts: similarProducts });
-=======
-      res.render('product-details', { product, similarProducts }); // Pass data to product-details.ejs
->>>>>>> 899ac96f48ee06a730cb707a43945c9b24c1f5d4
+      res.render("product-details", { product, similarProducts }); // Pass data to product-details.ejs
     });
   });
 });
 
-<<<<<<< HEAD
-
-    
-
-
-
-=======
->>>>>>> 899ac96f48ee06a730cb707a43945c9b24c1f5d4
 module.exports = router;
