76203e7f58e2f2badeeb8f36486240bbbd1a1724
Slade <>
2025-03-27 18:22:01 +0100

    shop now btn fixed

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -5,10 +5,11 @@ function Hero() {
     <section className="hero">
       <h1>Freaky Fashion</h1>
       <p>
-      Welcome to Freaky Fashion, where style meets bold creativity. Embrace the unconventional and stand out with our unique collections that redefine fashion. Get ready to make a statement!
+        Welcome to Freaky Fashion, where style meets bold creativity. Embrace the unconventional and stand out with our unique collections that redefine fashion. Get ready to make a statement!
       </p>
       <div className="hero-image">
         <img src="src/components/images/dressy_image.png" alt="Hero" />
+        <button className="shop-now-btn">SHOP NOW</button>
       </div>
     </section>
   );
